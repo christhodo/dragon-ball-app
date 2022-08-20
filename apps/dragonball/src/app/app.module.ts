79088197f452bd296c22f7a6a5939a376ea01198
christhodo <102981895+christhodo@users.noreply.github.com>
2022-08-20 09:55:30 -0700

    add libs files

--- apps/dragonball/src/app/app.module.ts
+++ apps/dragonball/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@dragon-ball-app/material';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { EnvironmentModule } from '@dragon-ball-app/environment';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClient,
+    EnvironmentModule.withEnvironment(environment),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
